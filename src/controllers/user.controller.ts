0e22fa65647aaf6b3c92440daa6bfdb6e2eef87d
Marcos Danilo <marcosdanilo94@gmail.com>
2022-08-01 15:10:27 -0300

    feat: :sparkles: Requisito 7 - Cria as validações para pessoas usuárias
    O endpoint faz validações dos campos enviados no corpo da request

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,8 +4,14 @@ import generateToken from '../utils/generateToken';
 
 const usersController = {
   async create(req: Request, res: Response) {
+    await userService.validateUserName(req.body.username);
+    await userService.validateClass(req.body.classe);
+    await userService.validateLevel(req.body.level);
+    await userService.validatePassword(req.body.password);
+
     const id = await userService.create(req.body);
     const token = await generateToken({ id, ...req.body });
+
     res.status(201).json({ token });
   },
 };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,41 @@
 import { User } from '../interfaces/user.interface';
 import userModel from '../models/user.model';
+import throwError from '../utils/throwError';
 
 const userService = {
   async create(newUser: User) {
     const id = await userModel.create(newUser);
 
     return id;
+  },
+  
+  async validateClass(classe: string) {
+    const lengthErrorMessage = '"classe" length must be at least 3 characters long';
+
+    if (!classe) throwError('400', '"classe" is required');
+    if (typeof classe !== 'string') throwError('422', '"classe" must be a string');
+    if (classe.length < 3) throwError('422', lengthErrorMessage);
+  },
+
+  async validatePassword(pass: string) {
+    const lengthErrorMessage = '"password" length must be at least 8 characters long';
+
+    if (!pass) throwError('400', '"password" is required');
+    if (typeof pass !== 'string') throwError('422', '"password" must be a string');
+    if (pass.length < 8) throwError('422', lengthErrorMessage);
+  },
+
+  async validateLevel(level: number) {
+    if (level < 1) throwError('422', '"level" must be greater than or equal to 1');
+    if (!level) throwError('400', '"level" is required');
+    if (typeof level !== 'number') throwError('422', '"level" must be a number');
+  },
+
+  async validateUserName(userName: string) {
+    const lengthErrorMessage = '"username" length must be at least 3 characters long';
+    if (!userName) throwError('400', '"username" is required');
+    if (typeof userName !== 'string') throwError('422', '"username" must be a string');
+    if (userName.length < 3) throwError('422', lengthErrorMessage);
   },
 };
 
